e0a9fdfb68f80d33eb72a8d603e48beeba02ce5e
Djeevs <contact@djeevs.com>
2021-05-24 00:35:55 +0200

    add static files

---
+++ mysite/static/js/typed.js
@@ -0,0 +1,10 @@
+    if (document.getElementById('typed')) {
+      var typed = new Typed("#typed", {
+        stringsElement: '#typed-strings',
+        typeSpeed: 90,
+        backSpeed: 90,
+        backDelay: 200,
+        startDelay: 500,
+        loop: true
+      });
+    }
